67543e609e504b41df4739f30842e4c3e190a4d6
Gabriel <gabrielperren@gmail.com>
2023-06-28 11:19:22 -0300

    updt code into new files

---
+++ 1_code/CANTAT20_fastMP_membs.py
@@ -0,0 +1,113 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+clpath = "/media/gabriel/rest/out/"
+final_dbs_path = "/home/gabriel/Github/UCC/add_New_DB/UCC_cat_20230620_out.csv"
+cantat20_DB_path = "/home/gabriel/Github/UCC/add_New_DB/databases/CANTAT20.csv"
+cantat20_membs_path = "../0_data/CG_2020_members.csv.gz"
+
+
+def main():
+    """
+    """
+    # Read cantat20 members data
+    print("Reading cantat20 members...")
+    cantat20_membs = pd.read_csv(cantat20_membs_path)
+    cantat20_ids = cantat20_membs['GaiaDR2']
+    cantat20_names = cantat20_membs['Cluster']
+
+    final_db = pd.read_csv(final_dbs_path)
+    cantat20_DB = pd.read_csv(cantat20_DB_path)
+
+    print("Processing clusters...")
+    ours_hist, cantat20_hist, match_hist = [np.zeros(12) for _ in range(3)]
+    for i, row in final_db.iterrows():
+
+        if 'CANTAT20' not in row['DB']:
+            continue
+        fname0 = row['fnames'].split(';')[0]
+
+        # Read fastMP data for this cluster
+        Qfold = row['quad']
+        try:
+            cl_d = pd.read_parquet(
+                clpath + Qfold + '/datafiles/' + fname0 + '.parquet')
+            probs = cl_d['probs'].values
+            msk = probs > 0.5
+            Nmembs = int(row['N_membs'])
+            if msk.sum() < Nmembs:
+                # Select the 'N_membs' stars with the largest probabilities
+                idx = np.argsort(probs)[::-1][:Nmembs]
+                msk = np.full(len(probs), False)
+                msk[idx] = True
+            cl_d = cl_d[msk]
+            msk = cl_d['Gmag'].values < 18
+            cl_d = cl_d[msk]            
+        except:
+            print(f"Could not find {Qfold}/{fname0}.parquet")
+            continue
+
+        # Identify cluster in cantat20 DB
+        dbs_idx = row['DB_i'].split(';')
+        h23_j = row['DB'].split(';').index('CANTAT20')
+        cantat20_idx = int(dbs_idx[h23_j])
+        cantat20_name = cantat20_DB['Cluster'][cantat20_idx].strip()
+
+        # Identify the members for this cluster in cantat20
+        if cantat20_name.startswith('FoF_'):
+            cantat20_name = cantat20_name.replace('FoF', 'LP')
+        if cantat20_name.startswith('VDBH_'):
+            cantat20_name = 'BH_' + cantat20_name.split(',')[0].split('_')[1]
+        if cantat20_name.startswith('VDB_'):
+            cantat20_name = 'vdBergh_' + cantat20_name.split(',')[0].split('_')[1]
+        msk = cantat20_names == cantat20_name
+        if msk.sum() == 0:
+            print(f"Could not find cluster {cantat20_name} in cantat20")
+            continue
+
+        # Store cantat20 magnitudes binned distribution
+        cantat20_gmag = cantat20_membs['Gmag'][msk].values
+        cantat20_hist += np.histogram(cantat20_gmag, bins=range(6, 19))[0]
+
+        # Store our magnitudes binned distribution
+        ours_gmag = cl_d['Gmag'].values
+        ours_hist += np.histogram(ours_gmag, bins=range(6, 19))[0]
+
+        # Store the matching magnitudes binned distribution
+        idx1 = np.where(np.isin(cl_d['Source'], cantat20_ids[msk]))[0]
+        match_gmag = ours_gmag[idx1]
+        # This is equivalent:
+        # idx2 = np.where(np.isin(cantat20_ids[msk], cl_d['Source']))[0]
+        # cantat20_gmag[idx2]
+        match_hist += np.histogram(match_gmag, bins=range(6, 19))[0]
+
+        N_ours, N_hunt, N_match = ours_gmag.size, cantat20_gmag.size,\
+            match_gmag.size
+        if N_ours+N_hunt > 0:
+            rel_diff = round((N_ours-N_hunt)/(N_ours+N_hunt), 3)
+        else:
+            rel_diff = 'nan'
+        print("{},{},{},{},{},{}".format(
+            i, fname0, N_ours, N_hunt, N_match, rel_diff))
+
+    print(match_hist)
+    print(ours_hist)
+    print(cantat20_hist)
+
+    x = np.arange(6.5, 18.5, 1)
+    plt.subplot(121)
+    plt.plot(x, match_hist/ours_hist, c='b')
+    plt.plot(x, match_hist/cantat20_hist, c='r')
+    plt.subplot(122)
+    plt.plot(x, (ours_hist - cantat20_hist) / ours_hist, marker='o', c='b')
+    plt.plot(x, (ours_hist - cantat20_hist) / cantat20_hist, marker='o', c='r')
+    plt.show()
+    breakpoint()
+
+
+if __name__ == '__main__':
+    # plt.style.use('science')
+    main()
---
+++ 1_code/fastMP_process.py
@@ -0,0 +1,80 @@
+
+import os
+import sys
+
+cluster_run = True
+try:
+    import call_fastMP
+except ModuleNotFoundError:
+    cluster_run = False
+
+if cluster_run is False:
+    # LOCAL RUN
+    # insert at 1, 0 is the script path (or '' in REPL)
+    # Path to modules
+    UCC_path = "/home/gabriel/Github/UCC/add_New_DB/"
+    sys.path.insert(1, UCC_path + 'modules/')
+    import call_fastMP
+    import main_process_GDR3_query as G3Q
+    # Path to fastMP
+    sys.path.insert(1, '/home/gabriel/Github/fastmp/')
+    from fastmp import fastMP
+    GAIADR3_path = '/media/gabriel/backup/gabriel/GaiaDR3/'
+    frames_path = GAIADR3_path + 'datafiles_parquet_G20/'
+    frames_ranges = GAIADR3_path + 'files_G20/frame_ranges.txt'
+    files = os.listdir(UCC_path)
+    for i, _ in enumerate(files):
+        if _.endswith("_in.csv"):
+            idx = i
+    UCC_fname = files[idx]
+    UCC_cat = UCC_path + UCC_fname
+    GCs_cat = UCC_path + "databases/globulars.csv"
+else:
+    # CLUSTER RUN
+    import call_fastMP
+    import main_process_GDR3_query as G3Q
+    from fastmp import fastMP
+    # Path to the database with Gaia DR3 data
+    GAIADR3_path = '/home/gperren/GaiaDR3/'
+    frames_path = GAIADR3_path + 'datafiles_parquet_G20/'
+    # File that contains the regions delimited by each frame (in this folder)
+    frames_ranges = GAIADR3_path + 'files_G20/frame_ranges.txt'
+    # Full database of clusters (in this folder)
+    files = os.listdir()
+    for i, _ in enumerate(files):
+        if _.endswith("_in.csv"):
+            idx = i
+    UCC_cat = files[idx]
+    GCs_cat = "globulars.csv"
+
+out_path = "out/"
+
+# Run ID
+# ij = int(os.path.basename(__file__).split('.')[0].split('_')[-1]) - 1
+try:
+    ij = int(sys.argv[1]) - 1
+except IndexError:
+    ij = -1
+
+
+def run(Nj=10):
+    """
+    """
+    # Read data
+    frames_data, df_UCC, df_gcs = call_fastMP.read_input(
+        frames_ranges, UCC_cat, GCs_cat)
+
+    # Split into N_j jobs
+    N_r = int(len(df_UCC) / Nj + 2)
+    if ij > 0:
+        clusters_list = df_UCC[ij*N_r:(ij + 1)*N_r]
+    else:
+        clusters_list = df_UCC
+
+    call_fastMP.run(
+        fastMP, G3Q, frames_path, frames_data, df_UCC, df_gcs, UCC_cat,
+        out_path, clusters_list)
+
+
+if __name__ == '__main__':
+    run()
